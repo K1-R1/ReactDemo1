91cc6e260ba306fe31fe730e405b84fb68570fc1
K1-R1 <kyranrawlinson@gmail.com>
2022-03-12 21:09:48 +0000

    Updated src/components/Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,13 @@
 function Main() {
     return (
-        <h1>main component</h1>
+        <main>
+            <h1 className="main--title">Facts about React</h1>
+            <ul className="main--facts">
+                <li>First released in 2013</li>
+                <li>Created by Jordan Walke</li>
+                <li>Maintained by Meta(Facebook)</li>
+            </ul>
+        </main>
     )
 }
 
